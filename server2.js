bcaeb2882ae4b651cfbf535927d93af635350f39
abubakarkamar19851988@gmail.com <124461119+1kamara@users.noreply.github.com>
2023-06-08 19:59:28 +0300

    nodejs db update

--- server2.js
+++ server2.js
@@ -7,12 +7,14 @@ import knex from 'knex';
 const db = knex({
     client: 'pg',
     connection:{
-        host: '127.0.0.1',
-        // connectionString: process.env.DATABASE_URL,y
-        user: 'postgres',
-        password: '',
-        database: 'great_brain',
-        // ssl: true, 
+        connectionString: process.env.DATABASE_URL,
+        ssl: {rejectUnauthorized: false}, 
+        user: process.env.DATABASE_HOST,
+        port:  5432,
+        user: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PW,
+        database: process.env.DATABASE_DB,
+      
     }
 
 });
